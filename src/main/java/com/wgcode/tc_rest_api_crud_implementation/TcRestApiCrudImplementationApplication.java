fa9dc0c2837c14ad824dbb9deee68b7be67012b7
gfizzle <winstongdouglas4@gmail.com>
2024-04-24 00:56:06 -0500

    TC REST API CRUD Implementation first commit

---
+++ src/main/java/com/wgcode/tc_rest_api_crud_implementation/TcRestApiCrudImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.wgcode.tc_rest_api_crud_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcRestApiCrudImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcRestApiCrudImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wgcode/tc_rest_api_crud_implementation/TcRestApiCrudImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgcode.tc_rest_api_crud_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcRestApiCrudImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
